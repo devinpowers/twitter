95483802089016e9f7484d5b474f357678fe096e
devinpowers <50432490+devinpowers@users.noreply.github.com>
2020-06-02 20:50:47 -0400

    Add files via upload

---
+++ Python_Grubhub_Twitter_Scrap.py
@@ -0,0 +1,46 @@
+''' Twitter Scrapper for Grubhub '''
+
+import json
+import csv
+import tweepy
+import re
+
+import secret_stuff
+
+consumer_key = secret_stuff.c_key
+consumer_secret = secret_stuff.Consumer_Secret
+access_token = secret_stuff.access_token
+access_token_secret = secret_stuff.access_token_secret
+
+
+def search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase):
+    
+    #create authentication for accessing Twitter
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    #initialize Tweepy API
+    api = tweepy.API(auth)
+    
+    #get the name of the spreadsheet we will write to
+    fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
+
+    #open the spreadsheet we will write to
+    with open('hastag%s.csv'%('grubhub'),'w') as file:
+        
+#    with open('%s.csv' % (fname), 'wb') as file:
+
+        w = csv.writer(file)
+
+        #write header row to spreadsheet
+        w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
+
+        #for each tweet matching our hashtags, write relevant info to the spreadsheet
+        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', \
+                                   lang="en", tweet_mode='extended').items(1000):
+            w.writerow([tweet.created_at, tweet.full_text.replace('\n',' ').encode('utf-8'), tweet.user.screen_name.encode('utf-8'), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count])
+            
+hashtag_phrase = input('Enter a Hashtag Phrase (Do not put #')
+
+if __name__ == '__main__':
+    search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase)
---
+++ secret_stuff.py
@@ -0,0 +1,12 @@
+
+# enter in
+
+
+c_key = "u715r5tK6D7gF2GJIzHTnvdcP"
+
+Consumer_Secret = "twWuRVgc3MWlcGxaaKtU5VmQEZW6xmkyZmewkRk16KVM7cdqmu"
+
+access_token = "1267949390968303616-sC9Gj5Z2HbV76KLTLwfuvcS1uNehz8"
+
+access_token_secret = "TaLZiqnxO4xi31zOD7fepBiLELOaldmoO31obv9lFiKOS"
+
