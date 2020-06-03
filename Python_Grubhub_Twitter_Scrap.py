cf9bc0f7d9e9aab9621007d723f807d0a546f1a0
devinpowers <50432490+devinpowers@users.noreply.github.com>
2020-06-02 21:17:10 -0400

    Update Python_Grubhub_Twitter_Scrap.py

--- Python_Grubhub_Twitter_Scrap.py
+++ Python_Grubhub_Twitter_Scrap.py
@@ -1,10 +1,8 @@
 ''' Twitter Scrapper for Grubhub '''
 
-import json
 import csv
 import tweepy
-import re
-
+# import tokens
 import secret_stuff
 
 consumer_key = secret_stuff.c_key
@@ -20,27 +18,26 @@ def search_for_hashtags(consumer_key, consumer_secret, access_token, access_toke
     auth.set_access_token(access_token, access_token_secret)
 
     #initialize Tweepy API
-    api = tweepy.API(auth)
-    
-    #get the name of the spreadsheet we will write to
-    fname = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
+    api = tweepy.API(auth)  
 
     #open the spreadsheet we will write to
-    with open('hastag%s.csv'%('grubhub'),'w') as file:
+   
+    with open('#Grubhub.csv','w') as file:
         
-#    with open('%s.csv' % (fname), 'wb') as file:
 
         w = csv.writer(file)
 
         #write header row to spreadsheet
         w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
 
-        #for each tweet matching our hashtags, write relevant info to the spreadsheet
-        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', \
-                                   lang="en", tweet_mode='extended').items(1000):
+        #for each tweet matching either the hashtag or the keyword, print information in row
+        # .items(#) indiciates the number of tweets we would like to grab
+        
+        for tweet in tweepy.Cursor(api.search, q=hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(10):
+            
             w.writerow([tweet.created_at, tweet.full_text.replace('\n',' ').encode('utf-8'), tweet.user.screen_name.encode('utf-8'), [e['text'] for e in tweet._json['entities']['hashtags']], tweet.user.followers_count])
             
-hashtag_phrase = input('Enter a Hashtag Phrase (Do not put #): ')
+hashtag_phrase = input('Enter a Hashtag Phrase or Keyword to search (#): ' )
 
 if __name__ == '__main__':
     search_for_hashtags(consumer_key, consumer_secret, access_token, access_token_secret, hashtag_phrase)
